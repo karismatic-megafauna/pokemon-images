7d77b4fe778f6d2d888cd25959db13c9273360b2
Michael Hinrichs <michaelghinrichs@gmail.com>
2017-05-10 11:34:49 -0700

    add squirtle

---
+++ index.js
@@ -0,0 +1,8 @@
+exports.pokemonSprites = {
+  squirtle: {
+    default: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png",
+  },
+  // 7: {
+  //   default: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png",
+  // },
+}
